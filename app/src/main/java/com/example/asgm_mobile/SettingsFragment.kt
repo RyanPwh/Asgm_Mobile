4cc997476fe026d9795e89ba598a1334ce491619
RyanPwh <ryanhao150906@gmail.com>
2023-05-01 12:04:48 +0800

    Assignment

--- app/src/main/java/com/example/asgm_mobile/SettingsFragment.kt
+++ app/src/main/java/com/example/asgm_mobile/SettingsFragment.kt
@@ -1,9 +1,11 @@
 package com.example.asgm_mobile
 
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.example.asgm_mobile.databinding.SettingActivityBinding
 import androidx.fragment.app.Fragment
@@ -24,7 +26,14 @@ class SettingsFragment : Fragment() {
             view.findNavController()
                 .navigate(R.id.action_settingsFragment_to_privacyPolicy)}
 
+        binding.langSetting.setOnClickListener{callToast()}
+
         return binding.root
     }
+
+    private fun callToast(){
+        Toast.makeText(context,"Please Change Your System Languages to Proceed!", Toast.LENGTH_LONG).show()
+    }
+
 
 }
