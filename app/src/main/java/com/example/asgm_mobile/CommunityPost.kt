704574b746bb6f436f3a3ed9120add9197977259
RyanPwh <ryanhao150906@gmail.com>
2023-05-04 15:42:37 +0800

    Assignment

--- app/src/main/java/com/example/asgm_mobile/CommunityPost.kt
+++ app/src/main/java/com/example/asgm_mobile/CommunityPost.kt
@@ -8,22 +8,18 @@ import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.asgm_mobile.databinding.CommunityBinding
+import com.example.asgm_mobile.databinding.CommunityPostBinding
 import com.example.asgm_mobile.databinding.SettingActivityBinding
 
-class Community : Fragment() {
-    private lateinit var binding: CommunityBinding
+class CommunityPost : Fragment() {
+    private lateinit var binding: CommunityPostBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): ConstraintLayout {
-        binding = CommunityBinding.inflate(inflater, container, false)
+        binding = CommunityPostBinding.inflate(inflater, container, false)
 
-        binding.fabBtn.setOnClickListener{add()}
+
 
         return binding.root
     }
-
-    private fun add(){
-
-    }
-
 
 }
