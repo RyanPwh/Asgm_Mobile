040f86de31caf340077c3aad8b5645f91be9ee30
RyanPwh <ryanhao150906@gmail.com>
2023-05-13 22:26:19 +0800

    Assignment

--- app/src/main/java/com/example/asgm_mobile/Community.kt
+++ app/src/main/java/com/example/asgm_mobile/Community.kt
@@ -7,16 +7,16 @@ import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.example.asgm_mobile.databinding.CommunityBinding
+import com.example.asgm_mobile.databinding.CommunitysBinding
 import com.example.asgm_mobile.databinding.SettingActivityBinding
 
 class Community : Fragment() {
-    private lateinit var binding: CommunityBinding
+    private lateinit var binding: CommunitysBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): ConstraintLayout {
-        binding = CommunityBinding.inflate(inflater, container, false)
+        binding = CommunitysBinding.inflate(inflater, container, false)
 
-        binding.fabBtn.setOnClickListener{add()}
+        binding.addBtn.setOnClickListener{add()}
 
         return binding.root
     }
